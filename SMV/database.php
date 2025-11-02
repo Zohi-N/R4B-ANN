13f6b730567b454df6d81823e0138bfa4007fe53
malijoza <alex.stropnik007@gmail.com>
2025-11-02 16:01:36 +0100

    Add files via upload

---
+++ SMV/database.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$host = "localhost";
+$db   = "mydb";
+$user = "myuser";
+$pass = "1234";
+$port = "5432";
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;options='--client_encoding=UTF8'";
+    $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    die("Database connection failed: " . htmlspecialchars($e->getMessage()));
+}
+?>
